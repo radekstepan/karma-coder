e0ec0e62212bfaadac82f1dc8a7c67bf1864c5a1
Radek Stepan <radek.stepan@gmail.com>
2011-07-16 21:03:02 +0100

    tag loading and parsing

--- models/tags.py
+++ models/tags.py
@@ -9,26 +9,27 @@ class Tags():
     tags = []
 
     def __init__(self):
-        # load
-        f = open(os.path.join('models/tags.yaml'))
-        tags = yaml.load(f)
-        f.close()
+        if not self.tags:
+            # load
+            f = open(os.path.join('models/tags.yaml'))
+            tags = yaml.load(f)
+            f.close()
 
-        # split
-        tags = tags.split()
+            # split
+            tags = tags.split()
 
-        # sort
-        tags.sort()
+            # sort
+            tags.sort()
 
-        # dictize
-        temp = {}
-        for tag in tags:
-            letter = tag[0]
-            if letter not in temp:
-                temp[letter] = []
-            temp[letter].append(tag)
+            # dictize
+            temp = {}
+            for tag in tags:
+                letter = tag[0]
+                if letter not in temp:
+                    temp[letter] = []
+                temp[letter].append(tag)
 
-        # listize
-        for key, value in temp.iteritems():
-            kv = [key, value]
-            self.tags.append(kv)
+            # listize
+            for key, value in temp.iteritems():
+                kv = [key, value]
+                self.tags.append(kv)
